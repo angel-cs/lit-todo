48814f00ea3b905953c7009e596fa81a01c1a041
acmg <acmg@MacBook-Pro-de-Angel.local>
2022-01-24 12:09:15 +0100

    added toggling

--- test/todo-app_test.js
+++ test/todo-app_test.js
@@ -39,14 +39,19 @@ describe('TodoApp', () => {
     element.tasks.should.deep.equal([firstTask]);
   })
 
-  it('completes todo by id arg', async () => {
+  it('new todo is undone and toogleTodo changes it', async () => {
     const element = await fixture(html`<todo-app></todo-app>`);
-
     element.tasks = [firstTask, secondTask];
-    await element.completeTodo(1);
-    element.tasks.find(task => task.id === firstTask.id).should.deep.equal({
-      ...firstTask, completed: true
-    })
+
+    await element.addTodo('tercera tarea');
+    const task = await element.tasks.find(t => t.task === 'tercera tarea')
+    task.completed.should.equal(false)
+
+    await element.toogleTodo(task.id)
+    task.completed.should.equal(true)
+
+    await element.toogleTodo(task.id)
+    task.completed.should.equal(false)
   })
 
   it('passes the a11y audit', async () => {
